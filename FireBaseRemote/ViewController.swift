4cc2faf4b9b2ef47eb1f92131508780e82273e98
Siwaporn Dee <moddedayja@gmail.com>
2017-05-19 11:28:11 +0700

    Initial Commit

---
+++ FireBaseRemote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireBaseRemote
+//
+//  Created by Siwaporn Dee on 5/19/2560 BE.
+//  Copyright © 2560 Siwaporn Dee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FireBaseRemoteTests/FireBaseRemoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  FireBaseRemoteTests.swift
+//  FireBaseRemoteTests
+//
+//  Created by Siwaporn Dee on 5/19/2560 BE.
+//  Copyright © 2560 Siwaporn Dee. All rights reserved.
+//
+
+import XCTest
+@testable import FireBaseRemote
+
+class FireBaseRemoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
